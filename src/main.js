1d37c6729aeac475d898f7a86e7fbd08a4901706
Samasameh9 <samaasamehh24@gmail.com>
2026-08-22 19:51:37 +0300

    .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-
+import db from "./DB/connection.db.js";
 import { globalErrorHandling } from "./middleware/error.middleware.js";
 import { authenticationController } from "./modules/authentication/index.js";
 import { blogController } from "./modules/blog/index.js";
@@ -10,7 +10,23 @@ const app = express();
 
 
 app.use(cors(), express.json());
+app.get("/test-db", async (req, res) => {
+  try {
+    const [result] = await db.execute("SELECT 1 + 1 AS result");
 
+    res.json({
+      message: "Database works",
+      result
+    });
+  } catch (error) {
+    console.error("DB ERROR:", error);
+
+    res.status(500).json({
+      message: "Database failed",
+      error: error.message
+    });
+  }
+});
 app.use("/auth",authenticationController);
 app.use("/blog",blogController);
 app.use("/user",userController);
@@ -22,6 +38,8 @@ app.get("/test", (req, res, next) => {
 app.all("{/*dummy}", (req, res, next) => {
   res.status(404).send({ message: "invalid application routing" });
 });
+
+
 app.use(globalErrorHandling)
 
 
