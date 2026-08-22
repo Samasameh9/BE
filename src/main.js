e7aabf567437f7fa97a79ffe460e88a71dc8a70b
Samasameh9 <samaasamehh24@gmail.com>
2026-08-22 19:54:03 +0300

    .

--- src/main.js
+++ src/main.js
@@ -10,29 +10,13 @@ const app = express();
 
 
 app.use(cors(), express.json());
-app.get("/test-db", async (req, res) => {
-  try {
-    const [result] = await db.execute("SELECT 1 + 1 AS result");
 
-    res.json({
-      message: "Database works",
-      result
-    });
-  } catch (error) {
-    console.error("DB ERROR:", error);
-
-    res.status(500).json({
-      message: "Database failed",
-      error: error.message
-    });
-  }
-});
 app.use("/auth",authenticationController);
 app.use("/blog",blogController);
 app.use("/user",userController);
 
 
-app.get("/test", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.send({ message: "welcome to BE API" });
 });
 app.all("{/*dummy}", (req, res, next) => {
