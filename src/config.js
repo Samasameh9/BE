d1c746690c0b883c5ea1afcae98973395dcae148
Samasameh9 <samaasamehh24@gmail.com>
2026-08-22 19:40:08 +0300

    .

--- src/config.js
+++ src/config.js
@@ -1,13 +1,15 @@
-import {config} from "dotenv"
-export const NODE_ENV=process.env.NODE_ENV??"development"
-config({path:`.env.${process.env.NODE_ENV??"development"}`})
+import { config } from "dotenv";
 
-export const PORT=parseInt(process.env.PORT??"9000")
-export const DB_NAME= process.env.DB_NAME
-export const DB_PORT= process.env.DB_PORT
-export const DB_HOST= process.env.DB_HOST
-export const DB_USER= process.env.DB_USER
-export const DB_PASSWORD= process.env.DB_PASSWORD
+if (process.env.NODE_ENV !== "production") {
+  config({ path: ".env.development" });
+}
 
+export const NODE_ENV = process.env.NODE_ENV ?? "development";
 
+export const PORT = parseInt(process.env.PORT ?? "9000");
 
+export const DB_NAME = process.env.DB_NAME;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
